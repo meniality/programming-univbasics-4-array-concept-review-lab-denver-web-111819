8ef634cbed21be59495ac14521bcab7bd1019f8b
patrick hoehn <patrh_99@hotmail.com>
2019-11-07 19:34:30 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -23,14 +23,16 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  counter = 0 
-  lowest_value = 99999999
+  lowest_value = 0
   
-  while counter < array.length do
-    if array[counter] < lowest_value
-      lowest_value = array[counter]
-    end
-  counter += 1 
+  array.length.times do |index|
+    if index == 0 
+      lowest_value = array[index]
+    else 
+      if array[index] < lowest_value
+        lowest_value = array[index]
+      end
+    end  
   end
   return lowest_value
 end
